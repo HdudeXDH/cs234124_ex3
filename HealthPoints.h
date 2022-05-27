da95b5eee6a4284e26d00eb41e2b6d9a9e7ad77c
alofeld@gmail.com <Depo2mypro!@#>
2022-05-27 13:42:21 +0300

    initiating

---
+++ HealthPoints.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 5/27/2022.
+//
+
+#ifndef EX3_HEALTHPOINTS_H
+#define EX3_HEALTHPOINTS_H
+
+#endif //EX3_HEALTHPOINTS_H
---
+++ Queue.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 5/27/2022.
+//
+
+#ifndef EX3_QUEUE_H
+#define EX3_QUEUE_H
+
+#endif //EX3_QUEUE_H
