248341d0880f32e2753685a7490d609c7280c184
alofeld@gmail.com <Depo2mypro!@#>
2022-05-30 17:44:36 +0300

    succeeded all unitests, including valgrind

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
-echo Begin testing
-
-#g++ --std=c++11 -o ex3_test -Wall -pedantic-errors -Werror -DNDEBUG *.cpp
-#valgrind --leak-check=full ./ex3_test
-
-#rm submission.zip
-#zip submission.zip Queue.h HealthPoints.h HealthPoints.cpp dry.pdf
-#~mtm/public/2122b/ex3/finalCheck submission.zip
+echo Begin testing----------
 
 make -f UnitTests/makefile && ./UnitTester && valgrind --leak-check=full ./UnitTester
+
+g++ --std=c++11 -o ex3_test -Wall -pedantic-errors -Werror -DNDEBUG TestMain.cpp HealthPointsExampleTest.cpp HealthPoints.cpp QueueExampleTests.cpp
+valgrind --leak-check=full ./ex3_test
+
+rm submission.zip
+zip submission.zip Queue.h HealthPoints.h HealthPoints.cpp dry.pdf
+~mtm/public/2122b/ex3/finalCheck submission.zip
