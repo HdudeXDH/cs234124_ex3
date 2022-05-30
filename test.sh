e62ebe7e10e2c2962d5e32f6384e76be0124c76b
alofeld@gmail.com <Depo2mypro!@#>
2022-05-30 14:22:12 +0300

    adding unitests

--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 echo Begin testing
 
-g++ --std=c++11 -o ex3_test -Wall -pedantic-errors -Werror -DNDEBUG *.cpp
-valgrind --leak-check=full ./ex3_test
+#g++ --std=c++11 -o ex3_test -Wall -pedantic-errors -Werror -DNDEBUG *.cpp
+#valgrind --leak-check=full ./ex3_test
 
-echo rm submission.zip
-echo zip submission.zip Queue.h HealthPoints.h dry.pdf
-echo ~mtm/public/2122b/ex3/finalCheck submission.zip
+#rm submission.zip
+#zip submission.zip Queue.h HealthPoints.h HealthPoints.cpp dry.pdf
+#~mtm/public/2122b/ex3/finalCheck submission.zip
+
+make -f UnitTests/makefile && ./UnitTester && valgrind --leak-check=full ./UnitTester
